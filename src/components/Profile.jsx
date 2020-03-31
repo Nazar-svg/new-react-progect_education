6d2e006de95c17355a13ebdba8d19f248feadfa5
Nazar <nazar.kolos90@gmail.com>
2020-03-31 13:59:02 +0300

    my new educational progect from useng  react

---
+++ src/components/Profile.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const Profile = () => {
+  return <div className="content">
+    <div>
+      <img src="https://uploads.elementcycles.net/2018/06/06235940/shutterstock_736432111-640x360.jpg" />
+    </div>
+    <div>
+      ava + discriphen
+    </div>
+    <div>
+      my post
+      <div>
+        new post
+      </div>
+      <div>
+        post 1
+      </div>
+      <div>
+        post 2
+      </div>
+    </div>
+  </div>;
+}
+
+export default Profile;
