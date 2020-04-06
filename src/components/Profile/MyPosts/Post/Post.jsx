e17955ad3086f79544b4baecf6b3baa4b9c0831f
Nazar <nazar.kolos90@gmail.com>
2020-04-06 22:40:27 +0300

    working stile posts

--- src/components/Profile/MyPosts/Post/Post.jsx
+++ src/components/Profile/MyPosts/Post/Post.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
 import classes from './Post.module.css';
+import img from './_PRO6169.JPG';
+import like from './heart.svg';
 const Post = (props) => {
   return (<div className={classes.post}>
+    <img className={classes.images} src={img}/>
     {props.message}
+    <button><img className={classes.like} src={like} alt="like"/></button>
   </div>);
 }
 
