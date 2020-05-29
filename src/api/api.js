b8e9ab6ed0a71fb2860acc94131aa372138b05a0
Nazar <nazar.kolos90@gmail.com>
2020-05-29 22:28:13 +0300

    refactoring server requests

--- src/api/api.js
+++ src/api/api.js
@@ -1,6 +1,13 @@
+import React from 'react';
 import * as axios from 'axios';
 
+
 export  const getUsers = (currentPage, pageSize ) => {
-    axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${currentPage}&count=${pageSize}`,
-    { withCredentials: true })
+  return axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${currentPage}&count=${pageSize}`,
+    {
+         withCredentials: true
+     })
+     .then(response => {
+       return response.data;
+        })
 }
--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -7,24 +7,22 @@ import Preloader from '../common/preloader/Preloader';
 import {getUsers} from '../../api/api';
 class UsersApiComponent extends React.Component {
 
-  componentDidMount() {
+  componentDidMount(props) {
     this.props.toggleIsFetching(true);
-    getUsers(this.props.currentPage, this.props.pageSize ).then(response => {
+    getUsers(this.props.currentPage, this.props.pageSize).then(data => {
         this.props.toggleIsFetching(false);
-        this.props.setUsers(response.data.items);
-        this.props.setTotalUsersCount(response.data.totalCount);
+        this.props.setUsers(data.items);
+        this.props.setTotalUsersCount(data.totalCount);
       });
   }
 
   onPageChanged = (pageNumber) => {
     this.props.toggleIsFetching(true);
     this.props.setCurrentPage(pageNumber);
-    axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`,
-      { withCredentials: true })
-      .then(response => {
+    getUsers(pageNumber, this.props.pageSize).then(data => {
         this.props.toggleIsFetching(false);
-        this.props.setUsers(response.data.items);
-        this.props.setTotalUsersCount(response.data.totalCount);
+        this.props.setUsers(data.items);
+        this.props.setTotalUsersCount(data.totalCount);
       });
   }
   render() {
